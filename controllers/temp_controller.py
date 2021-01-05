c475de7f81aa628fb259d6c022de7b415522d70c
Cedric <cedricremond@live.nl>
2021-01-05 19:54:16 +0100

    round to one decimal

--- controllers/temp_controller.py
+++ controllers/temp_controller.py
@@ -6,8 +6,8 @@ def get_temp_string():
     humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
 
     # humidity = round(humidity, 2)
-    temperature = round(temperature, 2)
-    return "{:.2f}".format(temperature)
+    temperature = round(temperature, 1)
+    return '{0:0.1f}'.format(temperature)
 
 def get_temp_float():
     humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
