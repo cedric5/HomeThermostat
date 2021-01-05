6f2f59229036d279998e63e04687733568522d13
Cedric <cedricremond@live.nl>
2021-01-05 18:10:57 +0100

    print some debug info

--- timer.py
+++ timer.py
@@ -10,18 +10,23 @@ import controllers.temp_controller as temp_controller
 def check_temps():
     thermostat_status = tools.get_config('thermostat_status')
     goal_temp = temp_controller.get_temp_float()
-
+    actual_temp = temp_controller.get_temp_float()
     if thermostat_status == 'open':
         offset_goal_temp = goal_temp - 0.8
     else:
         offset_goal_temp = goal_temp + 0.8
+        print('goal_temp:' + goal_temp)
+        print('offset_goal_temp:' + offset_goal_temp)
+        print('actual_temp:' + actual_temp)
 
-    if temp_controller.get_temp_float() >= offset_goal_temp:
+    if actual_temp >= offset_goal_temp:
         if thermostat_status == 'open':
+            print('closing')
             motor_controller.turn(-1, 0.25, 1)
             tools.write_config('thermostat_status', 'closed')
     else:
         if thermostat_status == 'closed':
+            print('opening')
             motor_controller.turn(1, 0.25, 1)
             tools.write_config('thermostat_status', 'open')
 
